212754f6ea3537f10d443a81d778641450b085fa
iamAliFBro <122121139+iamAliFBro@users.noreply.github.com>
2023-12-05 05:25:35 -0800

    fixed server.ts

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ async function dbConnect() {
     await mongoose.connect(config.db_url as string);
 
     app.listen(config.port, () => {
-      console.log(`server is running on port ${config.port}`);
+      console.log(`Running ${config.port}`);
     });
   } catch (error) {
     console.log(error);
